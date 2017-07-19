74d7f8d12b8b7e5961459ec1256fb9b6927be8af
Tim McIntire <tim@timmcintire.org>
2017-07-19 00:44:29 -0500

    prod files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -12,7 +12,7 @@ app.use(function (req, res, next){
   }
 });
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
